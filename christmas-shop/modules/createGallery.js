c683e1078a3bd0849999ec8840a69bc90adccb1f
Anna <yaroshenkoannette@gmail.com>
2024-10-18 00:42:13 +0200

    feat: add section best and creation gallery

---
+++ christmas-shop/modules/createGallery.js
@@ -0,0 +1,45 @@
+import { createElement } from "./createElement.js";
+
+const gallery = document.querySelector(".section__gallery");
+
+export function createGallery(arrGifts) {
+  arrGifts.forEach((gift, index) => {
+    const card = createElement({
+      tag: "div",
+      classes: ["card"],
+      parent: gallery,
+    });
+
+    const category = gift.category.split(" ")[1].toLowerCase();
+    card.setAttribute("data-number", `${category}_${index}`);
+
+    const image = createElement({
+      tag: "img",
+      classes: ["card__image"],
+      parent: card,
+    });
+
+    image.src = `./images/${category}.png`;
+    image.alt = category;
+
+    const container = createElement({
+      tag: "div",
+      classes: ["card__container"],
+      parent: card,
+    });
+
+    createElement({
+      tag: "h4",
+      text: gift.category,
+      classes: ["card__category", `card__category_${category}`],
+      parent: container,
+    });
+
+    createElement({
+      tag: "h3",
+      text: gift.name,
+      classes: ["card__title"],
+      parent: container,
+    });
+  });
+}
---
+++ christmas-shop/src/main.js
@@ -0,0 +1,4 @@
+import { createGallery } from "../modules/createGallery.js";
+import { gifts } from "../modules/gifts.js";
+
+createGallery([gifts[1], gifts[15], gifts[3], gifts[27]]);
