c1a401e67dbb2c2498c9fd1e8a8d449d2ea3eda9
Anna <yaroshenkoannette@gmail.com>
2024-12-02 20:57:18 +0100

    refactor: delete console.log

--- christmas-shop/src/burger.js
+++ christmas-shop/src/burger.js
@@ -5,7 +5,6 @@ burgerButton.addEventListener("click", showHideBurgerMenu);
 const page = document.querySelector("body");
 
 function showHideBurgerMenu(event) {
-  console.log(event.target);
   if (
     !burgerButton.classList.contains("burger-menu_active") &&
     !event.target.closest(".navigation__item")
@@ -21,7 +20,6 @@ function showHideBurgerMenu(event) {
   }
 
   function showMenu() {
-    console.log(1);
     burgerButton.classList.add("burger-menu_active");
     navigation.classList.add("navigation_active");
     page.classList.add("enabled-scroll");
