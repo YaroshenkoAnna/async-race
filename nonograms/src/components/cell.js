72cb8609aadb9657eb256706618215ddf380e9cd
Anna <yaroshenkoannette@gmail.com>
2025-01-26 21:32:09 +0100

    refactor: optimize cell.js

--- nonograms/src/components/cell.js
+++ nonograms/src/components/cell.js
@@ -6,50 +6,45 @@ export class Cell extends BaseElement {
     super({ tag: "div", classes: [styles.cell] });
     this.columnNumber = columnNumber;
     this.rowNumber = rowNumber;
-    this.addBoldBorder(columnNumber, rowNumber, size);
-    this.addListener("click", this.toggleCellFill.bind(this));
-    this.addListener("contextmenu", this.toggleCross.bind(this));
     this.isFilled = false;
     this.hasCross = false;
+
+    this.addBoldBorders(columnNumber, rowNumber, size);
+
+    this.addListener("click", this.toggleCellFill.bind(this));
+    this.addListener("contextmenu", this.toggleCross.bind(this));
   }
 
-  addBoldBorder(col, row, length) {
+  addBoldBorders(col, row, size) {
     const boldBorderEvery = 5;
-    if (col === 0) {
-      this.addClasses([styles.bl4]);
-    } else if (col === length - 1) {
-      this.addClasses([styles.br4]);
-    } else if ((col + 1) % boldBorderEvery === 0) {
-      this.addClasses([styles.br2]);
-    } else if (col % boldBorderEvery === 0) {
-      this.addClasses([styles.bl2]);
-    }
 
-    if (row === 0) {
-      this.addClasses([styles.bt4]);
-    } else if (row === length - 1) {
-      this.addClasses([styles.bb4]);
-    } else if ((row + 1) % boldBorderEvery === 0) {
-      this.addClasses([styles.bb2]);
-    } else if (row % boldBorderEvery === 0) {
-      this.addClasses([styles.bt2]);
-    }
+    const addBorderClass = (condition, style) => {
+      if (condition) this.addClasses([style]);
+    };
+
+    addBorderClass(col === 0, styles.bl4);
+    addBorderClass(col === size - 1, styles.br4);
+    addBorderClass((col + 1) % boldBorderEvery === 0, styles.br2);
+    addBorderClass(col % boldBorderEvery === 0, styles.bl2);
+
+    addBorderClass(row === 0, styles.bt4);
+    addBorderClass(row === size - 1, styles.bb4);
+    addBorderClass((row + 1) % boldBorderEvery === 0, styles.bb2);
+    addBorderClass(row % boldBorderEvery === 0, styles.bt2);
   }
 
   toggleCellFill() {
-    this.cross = false;
+    this.hasCross = false;
     this.setText("");
-    if (this.isFilled) {
-      this.isFilled = false;
-    } else {
-      this.isFilled = true;
-    }
+    this.isFilled = !this.isFilled;
     this.toggleClass(styles.filled);
   }
 
-  toggleCross() {
+  toggleCross(event) {
+    event.preventDefault();
     this.isFilled = false;
     this.removeClasses([styles.filled]);
+
     if (this.hasCross) {
       this.hasCross = false;
       this.setText("");
