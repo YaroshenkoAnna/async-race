7ec4cbb403241f712170898e9b3e8fc3335c4867
Anna <yaroshenkoannette@gmail.com>
2025-01-31 11:40:50 +0100

    refactor: optimize class modal

--- nonograms/src/components/modal.js
+++ nonograms/src/components/modal.js
@@ -6,26 +6,29 @@ import { formatTime } from "../utils/formatTime";
 export class Modal extends BaseElement {
   constructor() {
     super({ tag: "dialog", classes: [styles.modal] });
+
     this.addListener("close", () => {
       this.clearChildren();
     });
   }
-  button = new Button({
-    text: "ok",
-    classes: [styles.button],
-    callback: () => {
-      this.getNode().close();
-    },
-  });
+
+  createButton() {
+    return new Button({
+      text: "OK",
+      classes: [styles.button],
+      callback: () => this.getNode().close(),
+    });
+  }
 
   createWinModal(time) {
     const winTime = Math.ceil(time / 1000);
-    const p = new BaseElement({
+    const message = new BaseElement({
       tag: "p",
       classes: [styles.message],
       text: `Great! You have solved the nonogram in ${winTime} seconds!`,
     });
-    this.append(p, this.button);
+
+    this.append(message, this.createButton());
     this.getNode().showModal();
   }
 
@@ -39,48 +42,58 @@ export class Modal extends BaseElement {
       classes: [styles.title],
       text: "Rating",
     });
+
     const table = new BaseElement({ tag: "table", classes: [styles.table] });
     const thead = new BaseElement({
       tag: "thead",
       classes: [styles.tableTitles],
     });
-    const tr = new BaseElement({
+    const tbody = new BaseElement({ tag: "tbody", classes: [styles.table] });
+
+    const headers = ["#", "Time", "Puzzle", "Difficulty"];
+    const trHead = new BaseElement({
       tag: "tr",
       classes: [styles.tableTitles],
     });
-    thead.append(tr);
-    ["#", "Time", "Puzzle", "Difficulty"].forEach((e) => {
-      const th = new BaseElement({
-        tag: "th",
-        classes: [styles.tableTitles],
-        text: e,
-      });
-      tr.append(th);
+    headers.forEach((header) => {
+      trHead.append(
+        new BaseElement({
+          tag: "th",
+          classes: [styles.tableTitles],
+          text: header,
+        }),
+      );
     });
-    const tbody = new BaseElement({ tag: "tbody", classes: [styles.table] });
+    thead.append(trHead);
     table.append(thead, tbody);
 
-    container.append(title, table, this.button);
-    this.append(container);
-    const sortResults = results.sort((a, b) => a.time - b.time);
-    //обработать вариант, если массив пустой
-    sortResults.forEach((res, index) => {
-      const tr = new BaseElement({ tag: "tr", classes: [styles.item] });
-      tbody.append(tr);
-      [
-        `${index + 1}`,
-        `${formatTime(res.time)}`,
-        `${res.name}`,
-        `${res.difficulty}`,
-      ].forEach((e) => {
-        const th = new BaseElement({
-          tag: "th",
-          classes: [styles.item],
-          text: e,
-        });
-        tr.append(th);
+    if (results.length === 0) {
+      const emptyMessage = new BaseElement({
+        tag: "p",
+        classes: [styles.message],
+        text: "No results yet!",
       });
-    });
+      container.append(title, emptyMessage, this.createButton());
+    } else {
+      results.sort((a, b) => a.time - b.time);
+
+      results.forEach((res, index) => {
+        const tr = new BaseElement({ tag: "tr", classes: [styles.item] });
+        tbody.append(tr);
+
+        [index + 1, formatTime(res.time), res.name, res.difficulty].forEach(
+          (text) => {
+            tr.append(
+              new BaseElement({ tag: "td", classes: [styles.item], text }),
+            );
+          },
+        );
+      });
+
+      container.append(title, table, this.createButton());
+    }
+
+    this.append(container);
     this.getNode().showModal();
   }
 }
