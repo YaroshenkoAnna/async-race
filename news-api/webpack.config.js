e75ae1ae665a045afb2ada8e667621547d588e65
Anna Yaroshenko <yaroshenkoannette@gmail.com>
2025-02-17 20:23:06 +0100

    build: update TS, ESlint, webpack config files

--- news-api/webpack.config.js
+++ news-api/webpack.config.js
@@ -3,12 +3,17 @@ const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const DotenvWebpackPlugin = require('dotenv-webpack');
+const EslingPlugin = require('eslint-webpack-plugin');
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: path.resolve(__dirname, './src/index'),
     mode: 'development',
     module: {
         rules: [
+            {
+                test: /\.ts$/i,
+                use: 'ts-loader',
+            },
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
@@ -16,13 +21,14 @@ const baseConfig = {
         ],
     },
     resolve: {
-        extensions: ['.js'],
+        extensions: ['.ts', '.js'],
     },
     output: {
         filename: 'index.js',
         path: path.resolve(__dirname, './dist'),
     },
     plugins: [
+        new EslingPlugin({ extensions: 'ts' }),
         new DotenvWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, './src/index.html'),
