f74b6c437f352d70449343dbc57e592dd3dee982
Anna Yaroshenko <yaroshenkoannette@gmail.com>
2025-03-07 15:51:45 +0100

    build: add type checking rules

--- decision-making-tool/eslint.config.js
+++ decision-making-tool/eslint.config.js
@@ -1,45 +1,44 @@
+import eslint from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
-import * as typescriptPlugin from "@typescript-eslint/eslint-plugin";
 import parser from "@typescript-eslint/parser";
+import tseslint from "typescript-eslint";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+const __filename = String(fileURLToPath(import.meta.url));
 
-const typescriptConfig = {
-  parser: parser,
-  parserOptions: {
-    project: "./tsconfig.json", 
-    tsconfigRootDir: __dirname, 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+const __dirname = String(dirname(__filename));
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked, // Включаем правила с типовой проверкой
+  {
+    languageOptions: {
+      parser: parser,
+      parserOptions: {
+        project: "./tsconfig.json", // Указываем путь к tsconfig.json
+        tsconfigRootDir: __dirname,
+        projectService: true, // Разрешаем TypeScript анализировать файлы проекта
+        allowDefaultProject: true,
+      },
+    },
+
+    rules: {
+      "@typescript-eslint/consistent-type-assertions": [
+        "error",
+        { assertionStyle: "never" },
+      ],
+      "@typescript-eslint/consistent-type-imports": "error",
+      "@typescript-eslint/explicit-function-return-type": "error",
+      "@typescript-eslint/explicit-member-accessibility": [
+        "error",
+        { accessibility: "explicit", overrides: { constructors: "off" } },
+      ],
+      "@typescript-eslint/member-ordering": "error",
+      "class-methods-use-this": "error",
+    },
   },
-
-  plugins: {
-    "@typescript-eslint": typescriptPlugin,
-  },
-
-  linterOptions: {
-    noInlineConfig: true,
-    reportUnusedDisableDirectives: "warn",
-  },
-
-  rules: {
-    "@typescript-eslint/consistent-type-assertions": [
-      "error",
-      { assertionStyle: "never" },
-    ],
-    "@typescript-eslint/consistent-type-imports": "error",
-    "@typescript-eslint/explicit-function-return-type": "error",
-    "@typescript-eslint/explicit-member-accessibility": [
-      "error",
-      { accessibility: "explicit", overrides: { constructors: "off" } },
-    ],
-    "@typescript-eslint/member-ordering": "error",
-    "class-methods-use-this": "error",
-  },
-};
-
-export default [
-  typescriptConfig,
-  eslintConfigPrettier, 
-];
+  eslintConfigPrettier, // Подключаем Prettier как последнюю настройку
+);
