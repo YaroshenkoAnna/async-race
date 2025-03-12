601da8738c255ff63ae030d7ae851483cde6ca6a
Anna Yaroshenko <yaroshenkoannette@gmail.com>
2025-03-12 20:37:33 +0100

    feat: add button component

---
+++ src/components/button/button.ts
@@ -0,0 +1,20 @@
+import styles from "./button.module.scss";
+import { BaseElement } from "../../utils/base-element";
+
+type ButtonOptions = {
+  text: string;
+  classes: string[];
+  callback: () => void;
+};
+
+export class Button extends BaseElement<"button"> {
+  constructor(options: ButtonOptions) {
+    super({
+      tag: "button",
+      classNames: [styles.button],
+    });
+    this.setText(options.text);
+    this.addClasses(options.classes);
+    this.addListener("click", options.callback);
+  }
+}
