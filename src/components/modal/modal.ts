c847b8b8a74a6b48a8740f9eacad845edc21953b
Anna Yaroshenko <yaroshenkoannette@gmail.com>
2025-03-17 21:07:16 +0100

    feat: add blocking scroll, when modal is open and closing  cliching outside modal window

--- src/components/modal/modal.ts
+++ src/components/modal/modal.ts
@@ -2,17 +2,32 @@ import styles from "./modal.module.scss";
 import { BaseElement } from "../../utils/base-element";
 
 export class Modal extends BaseElement<"dialog"> {
+  public blockScroll: () => void;
+  public unblockScroll: () => void;
   constructor() {
     super({ tag: "dialog", classNames: [styles.modal] });
+    this.blockScroll = (): void => {
+      document.body.classList.add("no-scroll");
+    };
+    this.unblockScroll = (): void => {
+      document.body.classList.remove("no-scroll");
+    };
+    this.addListener("click", (event) => {
+      if (event.target === this.node) {
+        this.close();
+      }
+    });
   }
 
   public open(): void {
     document.body.append(this.node);
+    this.blockScroll();
     this.node.showModal();
   }
 
   public close(): void {
     this.node.close();
+    this.unblockScroll();
     this.deleteElement();
   }
 
